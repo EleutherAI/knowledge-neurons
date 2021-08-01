0897b7486398d354b633d891a250adf0577bbf78
sdtblck <sdtblck@gmail.com>
2021-08-01 11:55:36 +0000

    update pip packaging

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = [r.strip() for r in f.readlines()]
-
 name = 'knowledge-neurons'
 setup(
     name=name,
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='A library for finding knowledge neurons in pretrained transformer models',
     long_description=long_description,
@@ -18,7 +15,11 @@ setup(
     url=f'https://github.com/EleutherAI/{name}',
     author='Sid Black',
     author_email='sdtblck@gmail.com',
-    install_requires=[],
+    install_requires=["transformers",
+                        "einops",
+                        "numpy",
+                        "torch",
+                        "seaborn"],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
